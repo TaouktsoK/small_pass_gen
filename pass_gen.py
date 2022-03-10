dd560e77521fc74fba1fbd644d3be03b11fc1f9d
TaouktsoK <100849530+TaouktsoK@users.noreply.github.com>
2022-03-10 13:58:43 +0100

    Update pass_gen.py

--- pass_gen.py
+++ pass_gen.py
@@ -1,35 +1,6 @@
 import string
-import random
-
-
-class KeyGenSelector:
-    def __init__(self, full_char):
-        self.lower_letters = full_char[0]
-        self.upper_letters = full_char[1]
-        self.numbers = full_char[2]
-        self.special = full_char[3]
-
-    def lower_pick(self):
-        global lett
-        lett = random.choice(self.lower_letters)
-        return lett
-
-    def upper_pick(self):
-        global lett
-        lett = random.choice(self.upper_letters)
-        return lett
-
-    def number_pick(self):
-        global lett
-        lett = random.choice(self.numbers)
-        return lett
-
-    def special_pick(self):
-        global lett
-        lett = random.choice(self.special)
-        return lett
-
-# the above class picks letter from each category
+from mykeygenselector import KeyGenSelector
+from shuffler import shuffler
 
 lower_letters = string.ascii_lowercase
 upper_letters = string.ascii_uppercase
@@ -39,16 +10,15 @@ pass_length = 0
 full_char = [lower_letters, upper_letters, numbers, special]
 
 flag = True
-while flag:
-
-    print("""Recommended length for a password is at least ten characters,
+while flag == True:
+    print("""recommended length for a password is at least ten characters,
     if you want longer password, press the number of characters
-    else just press enter:""")
+    else just press enter""")
 
-    length_choise = input("Choose the length of password: ")
+    length_choise = input("choose length of password: ")
     while length_choise.isdigit() == False and length_choise != "":
-        print("Thats not a right password length.")
-        length_choise = input("Choose again the length of password: ")
+        print("not a correct length")
+        length_choise = input("choose length of password: ")
     if length_choise == "":
         pass_length = 11
     else:
@@ -67,10 +37,6 @@ while flag:
     num_range = 3
     special_range = 2
     counter = 1
-
-    """ in case of larger than default password
-    I add more characters of each category"""
-
     if pass_length >= 11:
         length_diff = pass_length - 10
         while length_diff > 0:
@@ -104,11 +70,8 @@ while flag:
         lett = keygen.special_pick()
         password_list.append(lett)
 
-    for i in range(5):
-        random.shuffle(password_list)
-    password = "".join(password_list)
-    print(password)
-    
+    shuffler(password_list)
+
     print("""if you want to quit, press q(and enter),
 if you want another password, just press enter. """)
     if input().lower() == "q":
